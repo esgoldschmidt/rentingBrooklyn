48bd7f1bac3e07ad4893721152334109fee53882
EGold <61056494+esgoldschmidt@users.noreply.github.com>
2020-07-06 22:09:52 -0400

    Updates

---
+++ precache-manifest.69d073dacad043895a5407dc4c300e7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869359470811de246305b6a8534bc334",
+    "url": "/rentingBrooklyn/index.html"
+  },
+  {
+    "revision": "802462a9d0f3e6b9a185",
+    "url": "/rentingBrooklyn/static/css/main.933a9479.chunk.css"
+  },
+  {
+    "revision": "6b6aade2985adc110c20",
+    "url": "/rentingBrooklyn/static/js/2.5430232e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rentingBrooklyn/static/js/2.5430232e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802462a9d0f3e6b9a185",
+    "url": "/rentingBrooklyn/static/js/main.c6558fd7.chunk.js"
+  },
+  {
+    "revision": "a503ec3ced825d3b6e13",
+    "url": "/rentingBrooklyn/static/js/runtime-main.0572ebda.js"
+  },
+  {
+    "revision": "759100ac3df65e7836f49ae9798fe11f",
+    "url": "/rentingBrooklyn/static/media/LogoSquare.759100ac.png"
+  },
+  {
+    "revision": "c06102a0d18113cf7b31cd8c887b2f71",
+    "url": "/rentingBrooklyn/static/media/LogoSquareWhite.c06102a0.png"
+  },
+  {
+    "revision": "5e7cf7905bfea29b8b7d6454dc9bf92c",
+    "url": "/rentingBrooklyn/static/media/contactImage.5e7cf790.png"
+  },
+  {
+    "revision": "10a75893c25c40d85b4e51ca61934811",
+    "url": "/rentingBrooklyn/static/media/mail.10a75893.png"
+  },
+  {
+    "revision": "562f99d4f473a5ed64e9bf47a7eb9484",
+    "url": "/rentingBrooklyn/static/media/map.562f99d4.png"
+  },
+  {
+    "revision": "a28bb90091987bcae36e8d588376f620",
+    "url": "/rentingBrooklyn/static/media/splashHome.a28bb900.png"
+  },
+  {
+    "revision": "8784cb962b0593b5efb51100b12ec2ed",
+    "url": "/rentingBrooklyn/static/media/workStation.8784cb96.jpg"
+  }
+]);
